4dcb0f88a4880882a7cba123c559eb0db7b60244
SATWIK SAI PRAKASH SAHOO <satwikspsahoo@gmail.com>
2020-06-13 00:03:23 +0000

    Add number_pyramid.java

---
+++ number_pyramid.java
@@ -0,0 +1,14 @@
+
+// Number Pyramid Pattern
+public class NumberPyramid {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            System.out.print(" ".repeat(rows - i));
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
