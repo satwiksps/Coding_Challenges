51ccd4de1c5d424496016685e16d5249f8ac4e36
SATWIK SAI PRAKASH SAHOO <satwikspsahoo@gmail.com>
2020-07-24 10:02:26 +0000

    Add merge_sort.js

---
+++ merge_sort.js
@@ -0,0 +1,23 @@
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+
+function merge(left, right) {
+    const result = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+    return [...result, ...left, ...right];
+}
+
+const arr = [38, 27, 43, 3, 9, 82, 10];
+console.log("Sorted array:", mergeSort(arr));
