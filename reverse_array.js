086d719154e1e24379d043d66e56e2cc8ab10485
SATWIK SAI PRAKASH SAHOO <satwikspsahoo@gmail.com>
2020-09-25 20:47:34 +0000

    Add reverse_array.js

---
+++ reverse_array.js
@@ -0,0 +1,9 @@
+// JavaScript program to reverse an array
+
+function reverseArray(arr) {
+    return arr.reverse();
+}
+
+// Example usage
+const numbers = [1, 2, 3, 4, 5];
+console.log("Reversed Array:", reverseArray(numbers));
