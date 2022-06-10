11e315d165fd119684292029be523d2c1713e14a
SATWIK SAI PRAKASH SAHOO <satwikspsahoo@gmail.com>
2022-06-10 20:23:06 +0000

    Add sort_numbers.py

---
+++ sort_numbers.py
@@ -0,0 +1,8 @@
+# Python program to sort a list of numbers in ascending order
+
+def sort_numbers(numbers):
+    return sorted(numbers)
+
+# Example usage
+nums = [5, 3, 8, 1, 2]
+print("Sorted Numbers:", sort_numbers(nums))
